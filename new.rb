6800d449961252f3cb5ee2e80d03fae201193e91
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-06-02 23:39:20 +0000

    [Sutdy]ruby

--- new.rb
+++ new.rb
@@ -1,4 +1,10 @@
 class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -8,15 +14,13 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
-
-car = Car.new
-car.run(5)
+car.move("右", 5)
 
 class Car
-  def self.run(distance)
-    puts "車で#{distance}キロ走ります。"
+  def 
+    self.turn(direction)
+    puts "#{direction}に曲がります。"
   end
 end
 
-Car.run(10)
+Car.turn("右")
